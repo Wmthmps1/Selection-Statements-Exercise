5ba83e2fe412c8b86e15a03145912f395fedbd84
William Thompson <wmthmps1@gmail.com>
2023-12-09 11:56:46 -0600

    Initial Commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -2,9 +2,37 @@
 {
     public class Program
     {
+        public static void GuessChecker(int myNumber, int userNumber){
+
+            if (userNumber < myNumber)
+            {
+                Console.WriteLine("too low");
+            }
+            else if (userNumber > myNumber)
+            {
+                Console.WriteLine("too high");
+            }
+            else // userNumber == myNumber
+            {
+                Console.WriteLine("You Guessed it!!!!");
+            }
+
+         }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            // Uncomment to easily test.
+            // Console.WriteLine(favNumber);
+
+            Console.WriteLine("Guess what my favorite number is: ");
+            Console.WriteLine("Guess a number between 1-1000");
+            var userInput = int.Parse(Console.ReadLine());
+
+            GuessChecker(favNumber,userInput);
         }
     }
 }
